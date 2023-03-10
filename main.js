d485753b6edad8242dba3441bc3d72b885b62fa5
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-03-10 11:01:46 +0100

    Ora 2,Gombok,kepek katintás focsz

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
-window.addEventListener("load", kezdes)
+window.addEventListener("load", kezdes);
 
-function kezdes(){ 
-    const ART = document.querySelectorAll("article");
-    ART[0].innerHTML = "<ul></ul>";
-    const UL = document.querySelectorAll("article ul");
-    UL[0].innerHTML += "<li></li>";
-    const KepLista = ["kep.jpg","kep2.jpg","kep3.jpg","kep4.jpg","kep5.jpg","kep6.jpg","kep7.jpg"];
-    for (let index = 0; index < KepLista.length; index++) {
-        const IMG = document.createElement("img");
-        IMG.src = KepLista[index];
-        ART[0].appendChild(IMG);
-        
-        
+let szamlalas = 0;
+
+function kezdes() {
+  const ART = document.querySelectorAll("article");
+  const KEPLISTA = [
+    "kep.jpg",
+    "kep2.jpg",
+    "kep3.jpg",
+    "kep4.jpg",
+    "kep5.jpg",
+    "kep6.jpg",
+    "kep7.jpg",
+  ];
+  for (let index = 0; index < KEPLISTA.length; index++) {
+    const IMG = document.createElement("img");
+    IMG.src = KEPLISTA[index];
+    ART[0].appendChild(IMG);
+  }
+
+  const KEPMOZGATAS = document.querySelectorAll("article img");
+  //KEPMOZGATAS.addEventListener
+  for (let i = 0; i < KEPLISTA.length; i++) {
+    console.log(KEPMOZGATAS[i]);
+    KEPMOZGATAS[i].addEventListener("click", function () {
+      IMGKepKezeles(KEPLISTA, i);
+    });
+  }
+
+  Gombok(KEPLISTA);
+}
+function IMGKepKezeles(KEPLISTA, i) {
+  let img = document.getElementsByClassName("kep")[0];
+  img.src = KEPLISTA[i];
+}
+function Gombok(KEPLISTA) {
+  const BALGOMBOK = document.getElementById("Bal");
+  const JOBBGOMBOK = document.getElementById("Jobb");
+  BALGOMBOK.addEventListener("click", function () {
+    szamlalas--;
+    if (szamlalas < 0) {
+      szamlalas = KEPLISTA.length - 1;
     }
 
+    document.getElementsByClassName("kep")[0].src = KEPLISTA[szamlalas];
+  });
+  JOBBGOMBOK.addEventListener("click", function () {
+    szamlalas++;
+    if (szamlalas >= KEPLISTA.length) {
+      szamlalas = 0;
+    }
 
-
+    document.getElementsByClassName("kep")[0].src = KEPLISTA[szamlalas];
+  });
 }
